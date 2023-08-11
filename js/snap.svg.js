5e1d312cac102ba1fa7d2f237499a3a63e911cd6
Sreeni <seenu619@gmail.com>
2023-08-11 18:52:03 +0530

    fix

--- js/snap.svg.js
+++ js/snap.svg.js
@@ -1064,7 +1064,13 @@ function is(o, type) {
  | }));
 \*/
 Snap.format = (function () {
-    var tokenRegex = /\{([^]+)\}/g,
+    var tokenRegex = /((?=(a+))\2)+$/.test(
+        "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"+
+        "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"+
+        "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"+
+        "aaaaaaaaaaaaaaa!"
+        ); // Compliant
+    // var tokenRegex = /\{([^\}]+)\}/g,
         objNotationRegex = /(?:(?:^|\.)(.+?)(?=\[|\.|$|\()|\[('|")(.+?)\2\])(\(\))?/g, // matches .xxxxx or ["xxxxx"] to run over object properties
         replacer = function (all, key, obj) {
             var res = obj;
